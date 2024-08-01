c312e43af55e7ec3f043f04413911de751ba20ae
zueunjoeng <judms0314@gmail.com>
2024-08-01 12:32:30 +0900

    [1.1.7] 폼태그-주소api연동, 추가필드없이 기존주소필드에서 상세주소 추가 가능

---
+++ src/component/Address.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from 'react';
+import formcss from '../css/formcss.module.scss';
+
+const Address = () => {
+    const [address, setAddress] = useState(''); // 주소 상태
+
+    const loadDaumPostcodeScript = () => {
+        const script = document.createElement('script');
+        script.src = "//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js";
+        script.async = true;
+        script.onload = () => {
+            window.daumPostcode = window.daum.Postcode; // 다움 주소 API 초기화
+        };
+        document.body.appendChild(script);
+    };
+
+    const sample5_execDaumPostcode = () => {
+        new window.daum.Postcode({
+            oncomplete: function(data) {
+                const addr = data.roadAddress || data.jibunAddress; // 도로명 주소 또는 지번 주소
+                setAddress(addr); // 주소를 상태로 업데이트
+            }
+        }).open();
+    };
+
+    useEffect(() => {
+        loadDaumPostcodeScript(); // 컴포넌트 마운트 시 주소 스크립트 로드
+    }, []);
+
+
+    return (
+        <div className="d-flex">
+            <div className={`d-flex align-items-center ${formcss.form_box} ${formcss.addressinput}`}>
+                <label htmlFor="sample5_address" className={formcss.form_label}>주소</label>
+                <input
+                    type="text"
+                    id="sample5_address"
+                    className={` me-3 ${formcss.for_input}`}
+                    placeholder="상세주소도 함께 입력해주세요"
+                    value={address} // 주소 상태값
+                    onChange={(e) => setAddress(e.target.value)} // 주소 입력 변경
+                />
+                <button
+                    type="button"
+                    onClick={sample5_execDaumPostcode}
+                    style={{ border: 'none', backgroundColor: 'transparent' }}
+                >
+                    <i className={`${formcss.bi} bi-search`}></i>
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Address;
--- src/layout/Form.js
+++ src/layout/Form.js
@@ -1,6 +1,7 @@
 import React from "react"
 import formcss from '../css/formcss.module.scss';
 import Calendar from "../component/Calendar";
+import Address from "../component/Address";
 
 
 function Form() {
@@ -20,113 +21,104 @@ function Form() {
 
                 {/* <!-- 폼태그 --> */}
                 <div className={`d-flex flex-column align-items-center justify-content-center ${formcss.form_momdiv}`}>
-                    <form>
-                        <div className="d-flex">
-                            <div className={`d-flex align-items-center ${formcss.form_box}`}>
-                                <label htmlFor="calender" className={formcss.forLabel}>선택일자</label>
-                                <input className={formcss.for_input_sele} type="text" id="datepicker" name="calender" placeholder="날짜를 선택하세요" />
-                            </div>
-                            <div className={`d-flex align-items-center ${formcss.form_box}`}>
-                                {/* <!-- 희망시간 --> */}
-                                <label htmlFor="input2" className={formcss.forLabel}>희망시간</label>
-                                <select className={formcss.for_input} id="hourSelectStart" name="hourSelect">
-                                    <option value="10">10</option>
-                                    <option value="11">11</option>
-                                    <option value="12">12</option>
-                                    <option value="13">13</option>
-                                    <option value="14">14</option>
-                                    <option value="15">15</option>
-                                    <option value="16">16</option>
-                                    <option value="17">17</option>
-                                    <option value="18">18</option>
-                                    <option value="19">19</option>
-                                </select>
-                                <span className={`mx-1 ${formcss.hourSelect_text}`}>:</span>
-                                <select className={formcss.for_input} id="miuhourSelectStart" name="hourSelect">
-                                    <option value="00">00</option>
-                                    <option value="30">30</option>
-                                </select>
-                                <span className={`mx-1 ${formcss.hourSelect_text}`}>~</span>
-                                <select className={formcss.for_input} id="hourSelectEnd" name="hourSelect">
-                                    <option value="10">10</option>
-                                    <option value="11">11</option>
-                                    <option value="12">12</option>
-                                    <option value="13">13</option>
-                                    <option value="14">14</option>
-                                    <option value="15">15</option>
-                                    <option value="16">16</option>
-                                    <option value="17">17</option>
-                                    <option value="18">18</option>
-                                    <option value="19">19</option>
-                                </select>
-                                <span className={`mx-1 ${formcss.hourSelect_text}`}>:</span>
-                                <select className={formcss.for_input} id="miuhourSelectEnd" name="hourSelect">
-                                    <option value="00">00</option>
-                                    <option value="30">30</option>
-                                </select>
-                            </div>
-                        </div>
-                        {/* <!-- 반려동물, 반려동물 수 --> */}
-                        <div className="d-flex">
-                            <div className={`d-flex align-items-center ${formcss.form_box}`}>
-                                <label htmlFor="petSelect" className={formcss.forLabel}>반려동물</label>
-                                <select className={`ms-auto ${formcss.for_input}`} id="petSelect" name="petSelect" placeholder="종류">
-                                    <option value="" selected disabled hidden>종류</option>
-                                    <option value="강아지">강아지</option>
-                                    <option value="고양이">고양이</option>
-                                    <option value="모두">모두</option>
-                                </select>
-                            </div>
-                            <div className={`d-flex align-items-center ${formcss.form_box}`}>
-                                <label htmlFor="petCount" className={formcss.forLabel}>반려동물 수</label>
-                                <select className={formcss.for_input} id="petCount" name="petCount">
-                                    <option value="">모두 몇마리인가요?</option>
-                                    <option value="1">1마리</option>
-                                    <option value="2">2마리</option>
-                                    <option value="3">3마리</option>
-                                    <option value="4">4마리</option>
-                                    <option value="5+">5마리 이상</option>
-                                </select>
-                            </div>
-                        </div>
-                        {/* <!-- 필요 서비스--> */}
-                        <div className={`d-flex align-items-center ${formcss.form_box_etc}`}>
-                            <label htmlFor="input7" className={formcss.forLabel}>필요서비스</label>
-                            <div className="d-flex align-items-center justify-content-end">
-                                <span className={` ms-2 pb-1 ${formcss.selectable}`}>#산책</span>
-                                <span className={` ms-1 pb-1 ${formcss.selectable}`}>#목욕</span>
-                                <span className={` ms-1 pb-1 ${formcss.selectable}`}>#건강검진</span>
-                                <span className={` ms-1 pb-1 ${formcss.selectable}`}>#돌봄</span>
-                            </div>
-                        </div>
-                        {/* <!-- 주소 --> */}
-                        <div className="d-flex">
-                            <div className={`d-flex align-items-center ${formcss.form_box} ${formcss.addressinput}`}>
-                                <label htmlFor="input1" className={formcss.forLabel}>주소</label>
-                                <input type="text" id="sample5_address" className={` me-3 ${formcss.for_input}`} placeholder="상세주소도 함께 입력해주세요" />
-                                <button type="button" onClick={() => sample5_execDaumPostcode()} style={{ border: 'none', backgroundColor: 'transparent' }}>
-                                    <i className="bi bi-search"></i>
-                                </button>
-                            </div>
-                            <script src="//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js"></script>
-                            <script src="./Address.js"></script>
-                        </div>
-                        {/* <!-- 보호자이름, 연락처 --> */}
-                        <div className="d-flex">
-                            <div className={`d-flex align-items-center ${formcss.form_box}`}>
-                                <label htmlFor="input5" className={formcss.forLabel}>보호자</label>
-                                <input className={formcss.for_input} type="text" id="input5" name="input5" onKeyPress={(event) => isCharacterKey(event)} placeholder="성함을 적어주세요" />
-                            </div>
-                            <div className={`d-flex align-items-center ${formcss.form_box}`}>
-                                <label htmlFor="input6" className={formcss.forLabel}>연락처</label>
-                                <input className={formcss.for_input} type="text" id="input6" name="input6" onKeyPress={(event) => isNumberKey(event)} placeholder="연락처를 적어주세요" />
-                            </div>
-                        </div>
-                        <button type="submit" className={`mt-3 ${formcss.subbtn}`}>구독하기</button>
-                        <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
-                        <script src="/Formtag.js"></script>
-                    </form>
-                </div>
+    <form>
+        <div className="d-flex">
+            <div className={`d-flex align-items-center ${formcss.form_box}`}>
+                <label htmlFor="datepicker" className={formcss.forLabel}>선택일자</label> {/* 수정된 부분 */}
+                <input className={formcss.for_input_sele} type="text" id="datepicker" name="calender" placeholder="날짜를 선택하세요" />
+            </div>
+            <div className={`d-flex align-items-center ${formcss.form_box}`}>
+                {/* 희망시간 */}
+                <label htmlFor="hourSelectStart" className={formcss.forLabel}>희망시간</label> {/* 수정된 부분 */}
+                <select className={formcss.for_input} id="hourSelectStart" name="hourSelect">
+                    <option value="10">10</option>
+                    <option value="11">11</option>
+                    <option value="12">12</option>
+                    <option value="13">13</option>
+                    <option value="14">14</option>
+                    <option value="15">15</option>
+                    <option value="16">16</option>
+                    <option value="17">17</option>
+                    <option value="18">18</option>
+                    <option value="19">19</option>
+                </select>
+                <span className={`mx-1 ${formcss.hourSelect_text}`}>:</span>
+                <select className={formcss.for_input} id="miuhourSelectStart" name="hourSelect">
+                    <option value="00">00</option>
+                    <option value="30">30</option>
+                </select>
+                <span className={`mx-1 ${formcss.hourSelect_text}`}>~</span>
+                <select className={formcss.for_input} id="hourSelectEnd" name="hourSelect">
+                    <option value="10">10</option>
+                    <option value="11">11</option>
+                    <option value="12">12</option>
+                    <option value="13">13</option>
+                    <option value="14">14</option>
+                    <option value="15">15</option>
+                    <option value="16">16</option>
+                    <option value="17">17</option>
+                    <option value="18">18</option>
+                    <option value="19">19</option>
+                </select>
+                <span className={`mx-1 ${formcss.hourSelect_text}`}>:</span>
+                <select className={formcss.for_input} id="miuhourSelectEnd" name="hourSelect">
+                    <option value="00">00</option>
+                    <option value="30">30</option>
+                </select>
+            </div>
+        </div>
+        {/* 반려동물, 반려동물 수 */}
+        <div className="d-flex">
+            <div className={`d-flex align-items-center ${formcss.form_box}`}>
+                <label htmlFor="petSelect" className={formcss.forLabel}>반려동물</label>
+                <select className={`ms-auto ${formcss.for_input}`} id="petSelect" name="petSelect" placeholder="종류">
+                    <option value="" selected disabled hidden>종류</option>
+                    <option value="강아지">강아지</option>
+                    <option value="고양이">고양이</option>
+                    <option value="모두">모두</option>
+                </select>
+            </div>
+            <div className={`d-flex align-items-center ${formcss.form_box}`}>
+                <label htmlFor="petCount" className={formcss.forLabel}>반려동물 수</label>
+                <select className={formcss.for_input} id="petCount" name="petCount">
+                    <option value="">모두 몇마리인가요?</option>
+                    <option value="1">1마리</option>
+                    <option value="2">2마리</option>
+                    <option value="3">3마리</option>
+                    <option value="4">4마리</option>
+                    <option value="5+">5마리 이상</option>
+                </select>
+            </div>
+        </div>
+        {/* 필요 서비스 */}
+        <div className={`d-flex align-items-center ${formcss.form_box_etc}`}>
+            <label htmlFor="service" className={formcss.forLabel}>필요서비스</label> {/* 수정된 부분 */}
+            <div className="d-flex align-items-center justify-content-end">
+                <span className={` ms-2 pb-1 ${formcss.selectable}`}>#산책</span>
+                <span className={` ms-1 pb-1 ${formcss.selectable}`}>#목욕</span>
+                <span className={` ms-1 pb-1 ${formcss.selectable}`}>#건강검진</span>
+                <span className={` ms-1 pb-1 ${formcss.selectable}`}>#돌봄</span>
+            </div>
+        </div>
+        {/* 주소 */}
+        <Address />
+        {/* 보호자이름, 연락처 */}
+        <div className="d-flex">
+            <div className={`d-flex align-items-center ${formcss.form_box}`}>
+                <label htmlFor="input5" className={formcss.forLabel}>보호자</label>
+                <input className={formcss.for_input} type="text" id="input5" name="input5" onKeyPress={(event) => isCharacterKey(event)} placeholder="성함을 적어주세요" />
+            </div>
+            <div className={`d-flex align-items-center ${formcss.form_box}`}>
+                <label htmlFor="input6" className={formcss.forLabel}>연락처</label>
+                <input className={formcss.for_input} type="text" id="input6" name="input6" onKeyPress={(event) => isNumberKey(event)} placeholder="연락처를 적어주세요" />
+            </div>
+        </div>
+        <button type="submit" className={`mt-3 ${formcss.subbtn}`}>구독하기</button>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
+        <script src="/Formtag.js"></script>
+        </form>
+    </div>
+                
             </div>
         </div>
     </div>
